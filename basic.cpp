0dfbc899e05c7da1d5042c9df3c7f3fa864ba31c
Nikhil <hackindosh@gmail.com>
2025-02-17 19:31:07 +0530

    added cin command

--- basic.cpp
+++ basic.cpp
@@ -1,8 +1,12 @@
 #include<iostream>
 using namespace std;
 int main(){
+    int cha;
     cout<<"hello world\n";
     cout<<"hello earth\n";
     cout<<"hello git";
+    cin>>cha;
+    cout<<cha;
+    
     return 0;
 }
